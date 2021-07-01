4cb508e19bc1108dd689a8be2267d7cfb0d1e802
Ryan Crowley <ryancrowleycode@gmail.com>
2021-07-01 16:03:46 -0500

    Queries for how to delete a table

---
+++ deleteTables.sql
@@ -0,0 +1,19 @@
+-- HOW TO DELETE TABLES FROM A DATABASE
+
+CREATE DATABASE example;
+
+SHOW DATABASES;
+
+USE example;
+
+CREATE TABLE test (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    age INT
+);
+
+SELECT * FROM test;
+SHOW TABLES;
+
+-- This is how you delete a table
+DROP TABLE test;

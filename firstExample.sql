370b0829df5df41d876d795d13bd92d65d16fff5
Ryan Crowley <ryancrowleycode@gmail.com>
2021-07-01 14:39:34 -0500

    first commit

---
+++ firstExample.sql
@@ -0,0 +1,15 @@
+-- Learning some basic commands and how to use MySQL Workbench
+-- This shows what databases you have
+SHOW DATABASES;
+
+-- This creates a database called "test"
+CREATE DATABASE test;
+
+-- This says to use the database that is called "test"
+USE test;
+
+-- This will show the tables in the database we are using
+SHOW TABLES;
+
+-- This will delete the database named "test"
+DROP DATABASE test;

46d2122ddd46d5ea71b6ce4d544c1728a5c74520
Ryan Crowley <ryancrowleycode@gmail.com>
2021-07-06 16:49:46 -0500

    how to rename a column

---
+++ renameColumn.sql
@@ -0,0 +1,10 @@
+USE test;
+
+-- How to rename a column (use back ticks, not single quotes)
+ALTER TABLE <tablename> CHANGE `old_column_name` `new_column_name` <data type>;
+
+SELECT * FROM pets;
+
+ALTER TABLE pets CHANGE `species` `animal_type` VARCHAR(20);
+
+ALTER TABLE pets CHANGE `animal_type` `species` VARCHAR(20);

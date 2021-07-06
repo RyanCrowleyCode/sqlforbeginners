24d7a51881022eb403023d76228a98236f7a6810
Ryan Crowley <ryancrowleycode@gmail.com>
2021-07-06 16:26:03 -0500

    Modified addingPrimaryAndForeignKeys to show queries for modifying primary and foreign keys

--- addingPrimaryAndForeignKeys.sql
+++ addingPrimaryAndForeignKeys.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE test;
-
+SHOW DATABASES;
 USE test;
 
 SHOW TABLES;
@@ -24,4 +24,37 @@ CREATE TABLE pets (
     species VARCHAR(20),
     owner_id INT
 );
+
+-- Shows the Fields, Types, Null, Key, Default, Extra
+DESCRIBE addresses;
+DESCRIBE people;
+DESCRIBE pets;
+
+-- Add a primary key to a table
+ALTER TABLE addresses
+ADD PRIMARY KEY (id);
+
+-- Remove primary key from a table
+ALTER TABLE addresses
+DROP PRIMARY KEY;
+
+ALTER TABLE people
+ADD PRIMARY KEY (id);
+
+ALTER TABLE pets
+ADD PRIMARY KEY (id);
+
+-- Add a foreign key to a table
+ALTER TABLE people
+ADD CONSTRAINT FK_PeopleAddress
+FOREIGN KEY (address_id) REFERENCES addresses(id);
+
+ALTER TABLE pets
+ADD CONSTRAINT FK_PetsPeople
+FOREIGN KEY (owner_id) references people(id);
+
+
+-- Remove a foreign key from a table BY CONSTRAINT NAME
+ALTER TABLE people
+DROP FOREIGN KEY FK_PeopleAddress;
 
